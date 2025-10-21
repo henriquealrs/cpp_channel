52a5bff896a6b545b71ad17d53407755123d3618
Henrique Lustosa <henriquealrs@gmail.com>
2025-10-21 16:58:49 -0300

    Test and some fixes

--- include/channel/channel.hpp
+++ include/channel/channel.hpp
@@ -1,69 +1,62 @@
-#ifndef CHANNEL_CHANNEL_HPP
-#define CHANNEL_CHANNEL_HPP
-
 #include <array>
+#include <atomic>
 #include <condition_variable>
-#include <cstddef>
 #include <mutex>
-#include <optional>
-#include <utility>
 
-template <typename T, int N = 0> class Channel {
-  static_assert(N > 0, "Channel capacity N must be greater than zero");
+template <typename T, int N = 0>
+class Channel {
+   public:
+    Channel() = default;
+    Channel(const Channel& other) = delete;
+    Channel& operator=(const Channel& other) = delete;
 
-public:
-  Channel() = default;
-  Channel(const Channel &other) = delete;
-  Channel &operator=(const Channel &other) = delete;
+   private:
+    std::atomic<int> counter_{0};
+    std::array<T, N> buffer_{};
+    std::mutex data_mutex_;
+    std::condition_variable cv_;
 
-private:
-  std::array<std::optional<T>, N> buffer_{};
-  std::size_t head_ = 0;
-  std::size_t tail_ = 0;
-  std::size_t count_ = 0;
-  std::mutex data_mutex_;
-  std::condition_variable data_available_;
-  std::condition_variable space_available_;
+   public:
+    void send(T& data) noexcept {
+        std::unique_lock<std::mutex> lk(data_mutex_);
+        cv_.wait(lk, [&cnt = counter_]() { return cnt < N; });
+        buffer_[counter_++] = std::move(data);
+        cv_.notify_all();
+    }
 
-  template <typename U> void enqueue(U &&data) {
-    std::unique_lock<std::mutex> lock(data_mutex_);
-    space_available_.wait(lock, [this]() { return count_ < N; });
+    void send(T&& data) noexcept {
+        std::unique_lock<std::mutex> lk(data_mutex_);
+        cv_.wait(lk, [&cnt = counter_]() { return cnt < N; });
+        buffer_[counter_++] = std::move(data);
+        cv_.notify_all();
+    }
 
-    buffer_[tail_] = std::forward<U>(data);
-    tail_ = (tail_ + 1) % N;
-    ++count_;
+    void send(const T& data) noexcept {
+        std::unique_lock<std::mutex> lk(data_mutex_);
+        cv_.wait(lk, [&cnt = counter_]() { return cnt < N; });
+        buffer_[counter_++] = data;
+        cv_.notify_all();
+    }
 
-    lock.unlock();
-    data_available_.notify_one();
-  }
+    void receive(T& data) noexcept {
+        std::unique_lock<std::mutex> lk(data_mutex_);
+        cv_.wait(lk, [&cnt = counter_] { return cnt > 0; });
+        data = std::move(buffer_[--counter_]);
+        cv_.notify_all();
+    }
 
-public:
-  void send(const T &data) { enqueue(data); }
+    void receive(T&& data) noexcept {
+        std::unique_lock<std::mutex> lk(data_mutex_);
+        cv_.wait(lk, [&cnt = counter_] { return cnt > 0; });
+        data = std::move(buffer_[--counter_]);
+        cv_.notify_all();
+    }
 
-  void send(T &&data) { enqueue(std::move(data)); }
-
-  void receive(T &data) {
-    std::unique_lock<std::mutex> lock(data_mutex_);
-    data_available_.wait(lock, [this]() { return count_ > 0; });
-
-    std::optional<T> &slot = buffer_[head_];
-    data = std::move(*slot);
-    slot.reset();
-
-    head_ = (head_ + 1) % N;
-    --count_;
-
-    lock.unlock();
-    space_available_.notify_one();
-  }
-
-  void operator<<(const T &data) { send(data); }
-
-  void operator<<(T &&data) { send(std::move(data)); }
+    void operator<<(const T& data) { send(data); }
+    void operator<<(T& data) { send(data); }
 };
 
-template <typename T, int N> void operator>>(Channel<T, N> &ch, T &data) {
-  ch.receive(data);
+template <typename T, int N>
+void operator>>(Channel<T, N>& ch, T& data) {
+    ch.receive(data);
 }
-
-#endif // CHANNEL_CHANNEL_HPP
--- test/test_channel_1.cpp
+++ test/test_channel_1.cpp
@@ -1,6 +1,6 @@
-#include <channel/channel.hpp>
-
 #include <atomic>
+#include <cassert>
+#include <channel/channel.hpp>
 #include <chrono>
 #include <iostream>
 #include <thread>
@@ -9,105 +9,107 @@
 namespace {
 
 bool basic_round_trip() {
-  Channel<int, 2> channel;
+    Channel<int, 2> channel;
 
-  int in = 42;
-  channel.send(in);
+    int in = 42;
+    channel.send(in);
 
-  int out = 0;
-  channel.receive(out);
-  return out == 42;
+    int out = 0;
+    channel.receive(out);
+    return out == 42;
 }
 
 bool move_semantics() {
-  Channel<std::vector<int>, 1> channel;
+    Channel<std::vector<int>, 1> channel;
 
-  std::vector<int> payload{1, 2, 3};
-  channel.send(std::move(payload));
+    std::vector<int> payload{1, 2, 3};
+    channel << std::move(payload);
 
-  std::vector<int> received;
-  channel.receive(received);
+    std::vector<int> received;
+    channel >> received;
 
-  return received.size() == 3 && received[0] == 1 && payload.empty();
+    return received.size() == 3 && received[0] == 1 && payload.empty();
 }
 
 bool blocking_behavior() {
-  Channel<int, 1> channel;
-  std::atomic<bool> second_send_started{false};
-  std::atomic<bool> second_send_completed{false};
-  std::atomic<int> second_value{0};
+    Channel<int, 1> channel;
+    std::atomic<bool> second_send_started{false};
+    std::atomic<bool> second_send_completed{false};
+    std::atomic<int> second_value{0};
 
-  int first = 1;
-  channel.send(first); // fills the buffer
+    int first = 1;
+    channel.send(first);  // fills the buffer
 
-  std::thread producer([&]() {
-    int value = 2;
-    second_send_started.store(true);
-    channel.send(value); // should block until a receive happens
-    second_send_completed.store(true);
-  });
+    std::thread producer([&]() {
+        int value = 2;
+        second_send_started.store(true);
+        channel.send(value);  // should block until a receive happens
+        second_send_completed.store(true);
+    });
 
-  std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
 
-  // second send should be waiting because buffer is full
-  bool waiting = second_send_started.load() && !second_send_completed.load();
+    // second send should be waiting because buffer is full
+    bool waiting = second_send_started.load() && !second_send_completed.load();
 
-  int observed_first = 0;
-  channel.receive(observed_first);
+    int observed_first = 0;
+    channel.receive(observed_first);
 
-  std::thread consumer([&]() {
-    int observed_second = 0;
-    channel.receive(observed_second);
-    second_value.store(observed_second);
-  });
+    std::thread consumer([&]() {
+        int observed_second = 0;
+        channel.receive(observed_second);
+        second_value.store(observed_second);
+    });
 
-  producer.join();
-  consumer.join();
+    producer.join();
+    consumer.join();
 
-  return waiting && observed_first == 1 && second_value.load() == 2;
+    return waiting && observed_first == 1 && second_value.load() == 2;
 }
 
 bool consistency(const int n = 200) {
-  std::atomic<int> cnt;
-  Channel<int, 3> ch;
-  auto producer_work = [&ch](int b, int e) {
-    for (int i = b; i < e; i++) {
-      std::cout << "sending " << i << "\n";
-      ch << i;
+    std::atomic<int> cnt;
+    Channel<int, 3> ch;
+    auto producer_work = [&ch](int b, int e) {
+        for (int i = b; i < e; i++) {
+            std::cout << "sending " << i << "\n";
+            ch.send(i);
+        }
+    };
+
+    std::thread producer1(producer_work, 0, n);  // , n / 2);
+    // std::thread producer2(producer_work, n / 2, n);
+
+    std::vector<int> nums(n);
+    auto consumer_work = [&nums, &ch]() {
+        int i = 0;
+        ch.receive(i);
+        std::cout << "Received " << i << "\n";
+        nums[i] = i;
+    };
+
+    std::thread consumer1(consumer_work);
+    // std::thread consumer2(consumer_work);
+
+    producer1.join();
+    // producer2.join();
+    consumer1.join();
+    // consumer2.join();
+    // consumer3.join();
+
+    bool res = true;
+    for (int i = 0; i < n; i++) {
+        res &= (nums[i] == i);
     }
-  };
-
-  std::thread producer1(producer_work, 0, n); // , n / 2);
-  // std::thread producer2(producer_work, n / 2, n);
-
-  std::vector<int> nums(n);
-  auto consumer_work = [&nums, &ch]() {
-    int i = 0;
-    ch >> i;
-    std::cout << "Received " << i << "\n";
-    nums[i] = i;
-  };
-
-  std::thread consumer1(consumer_work);
-  // std::thread consumer2(consumer_work);
-
-  producer1.join();
-  // producer2.join();
-  consumer1.join();
-  // consumer2.join();
-  // consumer3.join();
-
-  bool res = true;
-  for (int i = 0; i < n; i++) {
-    res &= (nums[i] == i);
-  }
-  return res;
+    return res;
 }
 
-} // namespace
+}  // namespace
 
 int main() {
-  bool ok = // basic_round_trip() && move_semantics() && blocking_behavior() &&
-      consistency(200);
-  return ok ? 0 : 1;
+    assert(basic_round_trip());
+    assert(move_semantics());
+    assert(blocking_behavior());
+    assert(consistency());
+    return 0;
 }
